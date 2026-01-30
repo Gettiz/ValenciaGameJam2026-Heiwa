8312be7ac2404ed4971de4f9c7e3a78dbfb1f43b
Jorge R. Hill <jorgerousseau80@gmail.com>
2026-01-30 18:57:26 -0400

    Added Materials Fix

---
+++ Assets/Scripts/ScriptsGettiz/FollowCameraPlayer.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class FollowCameraPlayer : MonoBehaviour
+{
+    private GameObject target;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
